ea9fa49085ef1aa55505bebd989955a3e9238ac5
SteveShim <steveshim496@gmail.com>
2024-01-04 22:35:32 +0900

    Add pictures

---
+++ src/components/sections/ImageGallery.tsx
@@ -0,0 +1,22 @@
+import Section from '@shared/Section'
+
+import styles from './Heading.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
+
+function ImageGallery({ images }: { images: string[] }) {
+  return (
+    <Section title="사진첩">
+      <ul className={cx('wrap-images')}>
+        {images.map((src, idx) => (
+          <li key={idx} className={cx('wrap-image')}>
+            <img src={src} alt="사진첩 이미지" />
+          </li>
+        ))}
+      </ul>
+    </Section>
+  )
+}
+
+export default ImageGallery
--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -8,7 +8,13 @@ const cx = classNames.bind(styles)
 function Video() {
   return (
     <Section className={cx('container')}>
-      <video autoPlay={true} muted={true} loop={true} controls={true}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        controls={true}
+        poster="/assets/poster.jpg"
+      >
         <source src="/assets/main.mp4" type="video/mp4" />
       </video>
     </Section>
