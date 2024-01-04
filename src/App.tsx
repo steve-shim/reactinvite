e8d923a122da3672c646f936953f37c54323035d
steve-shim <steveshim496@gmail.com>
2024-01-04 15:41:44 +0900

    Init Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,53 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 import classNames from 'classnames/bind'
+import { useEffect, useState } from 'react'
 
 import styles from './App.module.scss'
+
+import FullScreenMessage from './components/shared/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  return (
-    <div className={cx('container')}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  // 받아온 웨딩 데이터를 화면에 보여줘야하므로 상태값에 저장
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  // 1. wedding 데이터 호출
+  useEffect(() => {
+    setLoading(true)
+    // 비동기로부터 데이터 받아오는걸 보장받는 방법
+    // -> callback, promise, async/await
+    fetch('http://localhost:8000/wedding22') // 잘못된 주소로의 fetch는 404 에러가 뜬다
+      .then((response) => {
+        if (response.ok === false) {
+          throw new Error('청첩장 정보를 불러오지 못했습니다.')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        console.log('data', data)
+        setWedding(data)
+        //setLoading(false)
+      })
+      .catch((e) => {
+        console.log('에러발생', e)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false) // 성공하든 실패하든 무조건 수행
+      })
+  }, [])
+
+  if (loading) {
+    return <FullScreenMessage type="loading" />
+  }
+
+  if (error) {
+    return <FullScreenMessage type="error" />
+  }
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
 export default App
